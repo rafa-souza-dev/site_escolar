bc6147c182777d9c130e0ac8063b6de4260e07f7
Rafael <rafaeljosegomes25@hotmail.com>
2021-06-21 18:07:24 -0300

    novo arquivo requirements

--- progressao/settings.py
+++ progressao/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,6 @@ LOGIN_URL = 'login'
 # Arquivos de Media/Upload
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+# Hospedagem
+django_heroku.settings(locals())
