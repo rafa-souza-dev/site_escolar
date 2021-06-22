ef80bbd2a217b887f6efde085fbf6b5d41000e96
Rafael <rafaeljosegomes25@hotmail.com>
2021-06-22 13:48:10 -0300

    adicionando unique nos models

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Perfil(models.Model):
     nome_completo = models.CharField(max_length=70, null=True)
-    cpf = models.CharField(max_length=14, null=True, verbose_name="CPF")
-    telefone = models.CharField(max_length=16, null=True)
+    cpf = models.CharField(max_length=14, null=True, verbose_name="CPF", unique=True)
+    telefone = models.CharField(max_length=16, null=True, unique=True)
 
     usuario = models.OneToOneField(User, on_delete=models.PROTECT)
